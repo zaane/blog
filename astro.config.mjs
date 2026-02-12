dfd822be1aded0b77fc931bf4b21bb06a13e8908
zane <zaneolds@gmail.com>
2026-02-11 22:33:17 -0800

    start SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+const site = process.env.SITE_URL;
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site,
+});
